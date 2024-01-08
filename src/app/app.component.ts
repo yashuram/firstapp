8c817093ebd4b9f585c6a1cdc133bf7e7982702f
yashuram <yashuforu9274@gmail.com>
2024-01-08 11:19:22 +0530

    Adding first code change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,5 +10,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'firstapp';
+  title = 'Welcome to developers hub';
 }
